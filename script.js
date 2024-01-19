bc1578dc792532db795a729639edad4631678788
Vipul Dixit <dixit.vipul0511@gmail.com>
2024-01-19 16:38:44 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,72 +1,79 @@
 let choices = document.getElementsByClassName("options");
-const tools = ["rock","paper","scissors"];
+const tools = ["rock", "paper", "scissors"];
 var playerChoice;
-const modal =document.querySelector(".modal");
+const modal = document.querySelector(".modal");
 
 //for modal
-modal.addEventListener("click",()=>{
-	modal.style.display="none";
-})
+modal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
 
-function getComputerChoice(){
-	return tools[Math.floor(Math.random()*3)];
+function getComputerChoice() {
+  return tools[Math.floor(Math.random() * 3)];
 }
 
-function highlighComputer(computerChoice,color) {
-
-	document.getElementById(computerChoice).style.border = `3px solid ${color}`;
+function highlighComputer(computerChoice, color) {
+  document.getElementById(computerChoice).style.border = `3px solid ${color}`;
 }
 
-function resultOutput(player){
-	if(player=="player"){
-		document.querySelector(".main").style.transition=".2s";
-		document.querySelector(".main").style.background="radial-gradient(circle, rgba(0,0,0,1) 70%, rgba(132,252,70,1) 100%)";
-		
-	}else{
-		document.querySelector(".main").style.transition=".2s";
-		document.querySelector(".main").style.background="radial-gradient(circle, rgba(0,0,0,1) 70%, rgba(246,0,0,1) 100%)";
-		
-	}
+function resultOutput(player) {
+  if (player == "player") {
+    document.querySelector(".main").style.transition = ".2s";
+    document.querySelector(".main").style.background =
+      "radial-gradient(circle, rgba(0,0,0,1) 70%, rgba(132,252,70,1) 100%)";
+  } else {
+    document.querySelector(".main").style.transition = ".2s";
+    document.querySelector(".main").style.background =
+      "radial-gradient(circle, rgba(0,0,0,1) 70%, rgba(246,0,0,1) 100%)";
+  }
 }
 
-function playRound(player,computer){
-	console.log(player,computer)
-	highlighComputer(computer,"#FF3131")
-	let result = document.querySelector(".results")	
-	if(player==computer){
-		highlighComputer(computer,"#39FF14");
-		resultOutput("player");
-		result.textContent = "It's a tie!!";
-	}else if(player=="rock"){
-		if(computer=="paper"){
-			resultOutput("computer");
-			result.textContent = "Computer Wins!! Paper Defeats Rock";
-		}else{
-			resultOutput("player");
-			result.textContent = "Player Wins!! Rock Defeats Scissors";
-		}
-	}else if(player=="paper"){
-		if(computer=="scissors"){
-			resultOutput("computer");
-			result.textContent = "Computer Wins!! Scissors Defeats Paper";
-		}else{
-			resultOutput("player");
-			result.textContent = "Player Wins!! Paper Defeats Rock";
-		}
-	}else if(player=="scissors"){
-		if(computer=="rock"){
-			resultOutput("computer");
-			result.textContent = "Computer Wins!! Rock Defeats Scissors";
-		}else{
-			resultOutput("player");
-			result.textContent = "Player Wins!! Scissors Defeats Paper";
-		}
-	}
+function playRound(player, computer) {
+  console.log(player, computer);
+  highlighComputer(computer, "#FF3131");
+  let result = document.querySelector(".results");
+  if (player == computer) {
+    highlighComputer(computer, "#39FF14");
+    resultOutput("player");
+    result.textContent = "It's a tie!!";
+  } else if (player == "rock") {
+    if (computer == "paper") {
+      resultOutput("computer");
+      result.textContent = "Computer Wins!! Paper Defeats Rock";
+    } else {
+      resultOutput("player");
+      result.textContent = "Player Wins!! Rock Defeats Scissors";
+    }
+  } else if (player == "paper") {
+    if (computer == "scissors") {
+      resultOutput("computer");
+      result.textContent = "Computer Wins!! Scissors Defeats Paper";
+    } else {
+      resultOutput("player");
+      result.textContent = "Player Wins!! Paper Defeats Rock";
+    }
+  } else if (player == "scissors") {
+    if (computer == "rock") {
+      resultOutput("computer");
+      result.textContent = "Computer Wins!! Rock Defeats Scissors";
+    } else {
+      resultOutput("player");
+      result.textContent = "Player Wins!! Scissors Defeats Paper";
+    }
+  }
+  clearResponce(player, computer);
 }
-Array.from(choices).forEach((element)=>{
-	element.addEventListener("click",(e)=>{		
-		e.target.style.border="none";
-		e.target.style.border="3px solid #39FF14";
-		playRound(e.target.id,getComputerChoice())
-	})
+function clearResponce(player, computerChoice) {
+  setTimeout(() => {
+    document.querySelector(".main").style.background = "";
+    document.getElementById(computerChoice).style.border = "";
+    document.getElementById(player).style.border = "";
+  }, 3000);
+}
+Array.from(choices).forEach((element) => {
+  element.addEventListener("click", (e) => {
+    e.target.style.border = "none";
+    e.target.style.border = "3px solid #39FF14";
+    playRound(e.target.id, getComputerChoice());
+  });
 });
